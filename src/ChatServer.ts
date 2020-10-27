a141306db01e8ff75bc4823622a529f451d33593
salt-dev <salt-dev@admin-salts-MacBook-Pro.local>
2020-10-27 13:43:14 +0100

    Setting origin header

--- src/ChatServer.ts
+++ src/ChatServer.ts
@@ -24,6 +24,7 @@ export class ChatServer {
         }));
         this._app.use(createProxyMiddleware({ target: 'https://wizardly-mccarthy-d0859c.netlify.app', changeOrigin: true }))
         //this._app.options('*', cors());
+        this._app.set('Access-Control-Allow-Origin','https://wizardly-mccarthy-d0859c.netlify.app')
         this.server = createServer(this._app);
         this.initSocket();
         this.listen();
